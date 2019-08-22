556a14a910546566442f59b3d9965dc484171161
Jack <jack.scotson@netacea.com>
2019-08-22 12:09:50 +0100

    EVAL

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
+const uuid = require('uuid')
+const findOperativePairs = (command) => {
+  // '1d2+3d6*1d6'
+  const operators = ['*', '+', '/', '-']
+  let dice = {}
+  let temp = ''
+  let builtString = ''
+  let currentUuid = uuid.v4()
+  command.split('').forEach(i => {
+    if (!operators.includes(i)) {
+      temp += i
+    } else { // if continas the operator
+      dice[currentUuid] = temp
+      builtString += currentUuid
+      builtString += i
+      temp = ''
+      currentUuid = uuid.v4()
+    }
+  })
+  builtString += currentUuid
+  dice[currentUuid] = temp
+  return { dice, builtString }
+}
+
+const rollDice = (dice, rand) => {
+  const [roll, sides] = dice.split('d')
+  let tempResult = 0
+  for (let index = 0; index < roll; index++) {
+    tempResult += Math.floor((parseInt(sides.replace('d', ''))) * rand()) + 1
+  }
+  return tempResult
+}
+
 const diceRoller = (command, rand = Math.random) => {
-  return 4
+  let {builtString, dice} = findOperativePairs(command)
+  Object.keys(dice).forEach((key) => {
+    const value = rollDice(dice[key], rand)
+    builtString = builtString.replace(key, value)
+  })
+
+  return eval(builtString)
 }
 
 module.exports = diceRoller
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,5 +6,61 @@ describe('The dice roller', () => {
       const fakeRandom = () => 0.5
       expect(diceRoller('1d6', fakeRandom)).toEqual(4)
     })
+
+    it('should complete 2 simple rolls', () => {
+      const fakeRandom = () => 0.5
+      expect(diceRoller('2d6', fakeRandom)).toEqual(8)
+    })
+
+    it('should complete 2 simple rolls with 9 sides', () => {
+      const fakeRandom = () => 0.5
+      expect(diceRoller('2d9', fakeRandom)).toEqual(10)
+    })
+
+    it('should complete simple rolls and consider the random change', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('1d6', fakeRandom)).toEqual(2)
+    })
+
+    it('should complete simple rolls and consider the random change', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('3d6', fakeRandom)).toEqual(6)
+    })
+
+    it('should complete simple rolls and consider the random change', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('1d2', fakeRandom)).toEqual(1)
+    })
+
+    it('should complete more complex rolls and consider the random change', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('1d2*3d6', fakeRandom)).toEqual(6)
+    })
+
+    it('should complete even more complex rolls and consider the random change', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('2d2*3d6', fakeRandom)).toEqual(12)
+    })
+
+    it('should complete even more complex rolls and consider the random change', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('1d2+3d6', fakeRandom)).toEqual(7)
+    })
+
+    it('should complete a rolls with a multiplication & addition', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('1d2+3d6*1d6', fakeRandom)).toEqual(13)
+    })
+
+    it('should complete a roll with division', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('3d6/3d6', fakeRandom)).toEqual(1)
+    })
+
+    it('should complete a roll with subtraction', () => {
+      const fakeRandom = () => 0.25
+      expect(diceRoller('3d6-3d6', fakeRandom)).toEqual(0)
+    })
   })
 })
+
